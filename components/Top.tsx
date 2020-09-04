b13dd2349daf557c87d40622286c55fee195c299
NoCtrlZ <phantomofrotten@gmail.com>
2020-09-04 16:50:19 +0900

    introduce top and about pages

---
+++ components/Top.tsx
@@ -0,0 +1,8 @@
+const Top = () => (
+    <div>
+      <h1>Top</h1>
+      <p>This is the top page</p>
+    </div>
+)
+
+export default Top
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,35 @@
-import Link from 'next/link'
-import Header from '../components/Header'
+import React from "react"
+import Top from "../components/Top"
+import About from "../components/About"
 
-const IndexPage = () => (
-  <div>
-    <Header/>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </div>
-)
+interface AppProps {}
 
-export default IndexPage
+interface AppState {
+  path: string
+}
+
+export default class Index extends React.Component<AppProps, AppState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      path: '/'
+    }
+  }
+
+  setPath(path: string): void {
+    this.setState({path: path})
+  }
+
+  render() {
+    const { path } = this.state
+    return (
+      <div>
+        <h1>Hello Next.js 👋</h1>
+        <p onClick={() => this.setPath("/")}>Top</p>
+        <p onClick={() => this.setPath("/about")}>About</p>
+        <p>{path}</p>
+        {path === '/' ? <Top /> : <About />}
+      </div>
+    )
+  }
+}
